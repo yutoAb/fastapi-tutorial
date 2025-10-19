b30d187089d851766e899a5e68b75ef194b56643
yutoAb <yuto27abe@gmail.com>
2025-10-19 18:03:55 +0900

    初期設定と基本的なCRUD操作を持つFastAPIアプリケーションの構築

---
+++ api/schemas/task.py
@@ -0,0 +1,26 @@
+from typing import Optional
+
+from pydantic import BaseModel, Field
+
+
+class TaskBase(BaseModel):
+    title: Optional[str] = Field(None, example="クリーニングを取りに行く")
+
+
+class Task(TaskBase):
+    id: int
+    done: bool = Field(False, description="完了フラグ")
+
+    class Config:
+        orm_mode = True  # DB 接続に使用
+
+
+class TaskCreate(TaskBase):
+    pass
+
+
+class TaskCreateResponse(TaskCreate):
+    id: int
+
+    class Config:
+        orm_mode = True
